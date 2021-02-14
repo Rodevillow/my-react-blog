5b96c9f6c752e1c8b91087323adecc2c515920d1
Rodion <asvrodya@gmail.com>
2021-02-15 00:57:07 +0200

    Fix bug with password_confirmation on server side

--- server/authRouter.js
+++ server/authRouter.js
@@ -9,7 +9,7 @@ router.post('/registration', [
     check('email', 'Поле E-mail не может быть пустым!').notEmpty(),
     check('username', 'Имя пользователя не может быть пустым!').notEmpty(),
     check('password', 'Пароль должен быть не меньше 4-х и не больше 12-и символов!').isLength({min: 4, max: 12}),
-    check('password_confirmation', 'Пароли не совпадают').equals('password')
+    check('password_confirmation', 'Пароли не совпадают').custom((value, {req}) => (value === req.body.password)),
 ], controller.registration)
 
 router.post('/login', controller.login)
